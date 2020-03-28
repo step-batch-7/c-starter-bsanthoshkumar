56a2ace99cfcb75864165e9bacd31feaa2e56cf9
santhosh <santhosh.kumar@thoughtworks.com>
2020-03-28 15:50:08 +0530

    added cube funciton for finding cube of a number

--- basic_functions.c
+++ basic_functions.c
@@ -2,6 +2,7 @@
 int is_even(int);
 int is_odd(int);
 int square(int);
+int cube(int);
 
 int main(){
   int number;
@@ -10,6 +11,7 @@ int main(){
   printf(" %d is a %s number\n", number, is_even(number) ? "even" : "not even");
   printf(" %d is a %s number\n", number, is_odd(number) ? "odd" : "not odd");
   printf(" square of %d is a %d\n", number, square(number));
+  printf(" cube of %d is a %d\n", number, cube(number));
   return 0;
 }
 
@@ -23,4 +25,8 @@ int is_odd(int number) {
 
 int square(int number) {
   return number * number;
+}
+
+int cube(int number) {
+  return square(number) * number;
 }
