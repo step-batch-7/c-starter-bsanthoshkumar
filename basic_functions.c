957900b4fb7f43b5d3fef8fcee50b3bc562fcf10
santhosh <santhosh.kumar@thoughtworks.com>
2020-03-28 16:46:04 +0530

    added greatest_of_three function to find greatest of three numbers

--- basic_functions.c
+++ basic_functions.c
@@ -3,15 +3,20 @@ unsigned char is_even(int);
 unsigned char is_odd(int);
 int square(int);
 int cube(int);
+int find_greatest(int, int);
+int greatest_of_three(int, int, int);
 
-int main(){
-  int number;
+int main() {
+  int num1, num2, num3;
   printf("> Enter a number: ");
-  scanf("%d",&number);
-  printf(" %d is a %s number\n", number, is_even(number) ? "even" : "not even");
-  printf(" %d is a %s number\n", number, is_odd(number) ? "odd" : "not odd");
-  printf(" square of %d is a %d\n", number, square(number));
-  printf(" cube of %d is a %d\n", number, cube(number));
+  scanf("%d",&num1);
+  printf(" %d is a %s number\n", num1, is_even(num1) ? "even" : "not even");
+  printf(" %d is a %s number\n", num1, is_odd(num1) ? "odd" : "not odd");
+  printf(" square of %d is a %d\n", num1, square(num1));
+  printf(" cube of %d is a %d\n", num1, cube(num1));
+  printf("> Enter three numbers: ");
+  scanf("%d%d%d",&num1, &num2, &num3);
+  printf("Greatest of %d,%d,%d is %d",num1, num2, num3, greatest_of_three(num1, num2, num3));
   return 0;
 }
 
@@ -29,4 +34,11 @@ int square(int number) {
 
 int cube(int number) {
   return square(number) * number;
+}
+
+int find_greatest(int num1 , int num2) {
+  return num1 > num2 ? num1 : num2;
+}
+int greatest_of_three(int num1, int num2, int num3) {
+  return find_greatest(find_greatest(num1, num2), num3);
 }
