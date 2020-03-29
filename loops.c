4c1e79afab6a9f2c82946fe6f0a29b93011b9681
santhosh <santhosh.kumar@thoughtworks.com>
2020-03-29 10:13:06 +0530

    added fibonacci function which generates the fibonacci series of given length

--- loops.c
+++ loops.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 int factorial(int);
+int fibonacci(int);
 
 int main()
 {
   int number;
-  printf("> Enter a number: ");
+  printf("> Enter a number to find factorial: ");
   scanf("%d",&number);
   printf(" Factorial of %d is %d\n", number, factorial(number));
+  printf("> Enter a length to generate fibonacci series: ");
+  scanf("%d",&number);
+  fibonacci(number);
 }
 
 int factorial(int number)
@@ -17,4 +21,18 @@ int factorial(int number)
     result *= index;
   }
   return result;
+}
+
+int fibonacci(int length)
+{
+  int term1 = 0, term2 = 1;
+  printf(" Fibonacci series - ");
+  for(int index = 1;index <= length;index++)
+  {
+    printf("%d ", term1);
+    int temp = term2;
+    term2 += term1;
+    term1 = temp;
+  }
+  return 0;
 }
